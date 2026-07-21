dd705478f303ad36a2c9311e3dd4098435b1428c
MouazSurche <mouazemad681@gmail.com>
2026-07-21 10:52:43 +0300

    Fix netlify relative base path

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Portfolio/', // ئەم دێڕە لێرە زیاد بکە (ناوی کۆگاکەتە لە گیت‌هەب)
+  base: './',
 })
